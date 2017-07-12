62584d8fb6ed2c10e2f6ec894f18dcf248e1ff6b
zarzourii <imad.zarzouri@keyrus.com>
2017-07-12 22:36:44 +0200

    init zainat application

--- src/main/java/com/zainat/ZainatApplication.java
+++ src/main/java/com/zainat/ZainatApplication.java
@@ -1,5 +1,6 @@
 package com.zainat;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import org.springframework.batch.core.Job;
@@ -11,37 +12,39 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.zainat.service.ZainatService;
 
 @SpringBootApplication
-public class ZainatApplication /*implements CommandLineRunner*/ {
+public class ZainatApplication implements CommandLineRunner {
 
 	@Autowired
 	ZainatService zainatServiceImpl;
 
 	public static void main(String[] args) {
-//		SpringApplication.run(ZainatApplication.class, args);
+		SpringApplication.run(ZainatApplication.class, args);
 		
-		ClassPathXmlApplicationContext cpt = new ClassPathXmlApplicationContext("jobContext-operation.xml");
-        cpt.start();
-        JobLauncher jobLauncher = (JobLauncher) cpt.getBean("jobLauncher");
-        Job job = (Job) cpt.getBean("operationExtraction");
-        JobParameters parameter = new JobParametersBuilder().toJobParameters();
-        try {
-			jobLauncher.run(job, parameter);
-		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
-				| JobParametersInvalidException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		ClassPathXmlApplicationContext cpt = new ClassPathXmlApplicationContext("jobContext-operation.xml");
+//        cpt.start();
+//        JobLauncher jobLauncher = (JobLauncher) cpt.getBean("jobLauncher");
+//        Job job = (Job) cpt.getBean("operationExtraction");
+//        JobParameters parameter = new JobParametersBuilder().toJobParameters();
+//        try {
+//			jobLauncher.run(job, parameter);
+//		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
+//				| JobParametersInvalidException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 	}
 
-//	@Override
-//	public void run(String... ags) throws ParseException {
-//		zainatServiceImpl.process();
-//	}
+	@Override
+	public void run(String... ags) throws ParseException {
+		zainatServiceImpl.process();
+	}
 }
